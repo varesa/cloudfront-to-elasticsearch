f2d066ccd37a4210a788cc5c7e897eb8d9d79042
Esa Varemo <esa@kuivanto.fi>
2021-01-23 01:59:39 +0200

    Cleanup

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,33 @@
-use std::env;
-use std::fs;
-use std::collections::HashMap;
-use itertools::Itertools;
 use elasticsearch::{
-//    cat::CatIndicesParts,
     http::{
         request::JsonBody,
         transport::Transport,
     },
-//    indices::{
-//        IndicesCreateParts, IndicesDeleteParts, IndicesExistsParts, IndicesPutSettingsParts,
-//    },
     BulkParts, Elasticsearch,
 };
+use itertools::Itertools;
 use ring::digest;
+use serde_json::Value;
+use std::env;
+use std::fs;
+use std::collections::HashMap;
 
 #[macro_use]
 extern crate serde_json;
-use serde_json::Value;
+
+fn update_keys_from_header(line: &str, keys: &mut Vec<String>) {
+    let split_header = line.split(' ').collect::<Vec<&str>>();
+    if split_header[0] == "#Fields:" {
+        // Since the files being parsed could be concatenations over several years,
+        // refresh the array of keys every time a new header is encountered.
+        keys.clear();
+        for field_name in split_header[1..].iter() {
+            keys.push(
+                field_name.replace("(", "-").replace(")", "")
+            );
+        }
+    }
+}
 
 #[tokio::main]
 pub async fn main() -> Result<(), Box<dyn std::error::Error>> {
@@ -28,9 +38,6 @@ pub async fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     let transport = Transport::single_node(&args[1]).expect("Unable to create ES transport");
     let client = Elasticsearch::new(transport);
-
-    //let response = client.cat().indices(CatIndicesParts::None).send().await?;
-    //println!("{:?}", response.text().await?);
 
     const CHUNK_SIZE: usize = 100;
 
@@ -45,30 +52,18 @@ pub async fn main() -> Result<(), Box<dyn std::error::Error>> {
     for chunk in &chunks {
         let mut body: Vec<JsonBody<_>> = Vec::with_capacity(2*CHUNK_SIZE);
         for line in chunk {
-            // Skip headers
-            if line.chars().nth(0) == Some('#') {
-                let parts = line.split(' ').collect::<Vec<&str>>();
-                if parts[0] == "#Fields:" {
-                    // Since the files being parsed could be concatenations over several years,
-                    // refresh the array of keys every time a new header is encountered.
-                    keys.clear();
-                    for field_name in parts[1..].iter() {
-                        keys.push(
-                            field_name.replace("(", "-").replace(")", "")
-                        );
-                    }
-                }
-            } 
+            update_keys_from_header(line, &mut keys);
 
             let hash = digest::digest(&digest::SHA256, line.as_bytes());
             body.push(json!({"index": {
                 "_id": format!("{:x?}", hash)[7..18],
             }}).into());
-            let data = keys.iter().zip(line.split('\t')).collect::<HashMap<_, _>>();
-            body.push(json!(data).into());
-            /*body.push(json!({
-                "line": line,
-            }).into());*/
+
+            body.push(json!(
+                keys.iter()
+                    .zip(line.split('\t'))
+                    .collect::<HashMap<_, _>>()
+            ).into());
         }
         let response = 
             client
@@ -79,7 +74,6 @@ pub async fn main() -> Result<(), Box<dyn std::error::Error>> {
         let response_body = response.json::<Value>().await?;
         let successful = response_body["errors"].as_bool().unwrap() == false;
         assert!(successful, format!("Error from Elasticsearch: {:?}", response_body));
-
     }
 
     Ok(())
